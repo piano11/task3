8afd61e2c3a1159c283e7470c2126efd0e295c20
Andrey <andrey.fedotof@yandex.ru>
2022-08-20 09:35:38 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int cube = 1;
+while (cube <= numberN)
+{   
+   int result = cube * cube * cube;
+   Console.Write (" " + result);
+     cube++;
+ }
